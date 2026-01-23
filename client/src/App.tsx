d31ad94a3d2edec5bc200c1d191d7dd95d0a1aef
electr1fy0 <electr1fy0@icloud.com>
2026-01-23 09:47:10 +0530

    add some ui over the qns

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,55 @@
+import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "./components/ui/card";
+import { Input } from "./components/ui/input";
 import { useGetQuestions } from "./hooks/questions";
 
 export function App() {
   const { data: questions = [], isLoading } = useGetQuestions();
 
   if (isLoading) return <div>Loading...</div>;
-  return <div>resp: {JSON.stringify(questions)}</div>;
+  const handleSubmit = () => {};
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen">
+      <header className="w-full max-w-lg my-8 mt-24">
+        <h1 className="w-full max-w-lg  text-center text-3xl">Unsaid</h1>
+        <h2 className="w-full max-w-lg text-neutral-500 text-center">
+          Say unsaid things without fear
+        </h2>
+      </header>
+
+      <div className="w-full max-w-lg">
+        <h2 className="w-full max-w-lg text-neutral-700">
+          Unanswered Messages
+        </h2>
+        {questions.map((q) => {
+          return (
+            <Card key={q.id} className="my-4">
+              <CardHeader>{q.content}</CardHeader>
+              <CardContent>
+                <form onSubmit={handleSubmit}>
+                  <Input
+                    type="text"
+                    placeholder="Enter your reply..."
+                    required
+                  ></Input>
+                </form>
+              </CardContent>
+              <CardFooter>
+                <Button type="submit" variant="default" className="hov">
+                  Reply
+                </Button>
+              </CardFooter>
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
